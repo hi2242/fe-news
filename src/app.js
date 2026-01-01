446d817da40302b5bd8e69df62ec9a87bb777961
윤종근 <doodukly1999@gmail.com>
2026-01-01 23:05:10 +0900

    style(view): 전체 뷰 구성

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import { reload } from '@/js/Reload.js';
 import TitleLogo from '@/assets/images/light_mode_logo/title_logo.png';
 
 document.querySelector('#app').innerHTML = `
-  <div class="upperTab">
-    <button id="title" type="button">
-      <img src="${TitleLogo}" class="newspaperIcon">
-      <div class="displayText">뉴스스탠드</div>
-    </button>
-    <div class="todaysDate">
-      ${getDate()}
+  <div id="view">
+    <div class="upperTab">
+      <button id="title" type="button">
+        <img src="${TitleLogo}" class="newspaperIcon">
+        <div class="displayText">뉴스스탠드</div>
+      </button>
+      <div class="todaysDate">
+        ${getDate()}
+      </div>
+    </div>
     </div>
   </div>
 `
